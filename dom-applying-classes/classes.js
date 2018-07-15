0aae6171e19d8ebd218f7bea27a24967bae34a68
tommy-hall <thall2906@gmail.com>
2018-07-15 14:56:40 +1200

    Dom Applying Classes

---
+++ dom-applying-classes/classes.js
@@ -0,0 +1,50 @@
+// Don't change or delete this line! It waits until the DOM has loaded, then calls 
+// the start function. More info: 
+// https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
+document.addEventListener('DOMContentLoaded', start)
+
+function start () {
+  // The first example is done for you. Uncomment the line below and reload the browser.
+  one()
+  two()
+  three()
+  makeVisible()
+  // Your turn! Create a new function called `two`, then call it from here.
+}
+
+function one () {
+  // First, we have to find the element:
+  var one = document.getElementById('one')
+
+  // Next, we apply a new CSS class to it:
+  one.classList.add('blue')
+}
+
+// CREATE FUNCTION two HERE
+
+function two () {
+  // First, we have to find the element:
+  var two = document.getElementById('two')
+
+  // Next, we apply a new CSS class to it:
+  two.classList.add('green')
+}
+
+// CREATE FUNCTION three HERE
+function three () {
+  // First, we have to find the element:
+  var three = document.getElementById('three')
+
+  // Next, we apply a new CSS class to it:
+  three.classList.add('orange')
+}
+
+
+// CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+  // First, we have to find the element:
+  var four = document.getElementsByClassName('invisible')[0]
+
+  // Next, we apply a new CSS class to it:
+  four.classList.add('visible')
+}
